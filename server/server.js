378176ef9986a47a5a0af2b489149649348f210e
Darko Tasevski <c.dtasevski@gogoair.com>
2018-08-10 12:52:36 +0200

    Add initial web sockets config

--- server/server.js
+++ server/server.js
@@ -1,17 +1,31 @@
+const http = require('http');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const morgan = require('morgan');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
+const PUBLIC_PATH = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(PUBLIC_PATH));
+
+io.on('connection', socket => {
+	console.log('New user connection!');
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
 
 app.get('/', (req, res) => {
 	res.send('Hello');
 });
 
-app.listen(PORT, () => console.log(`Server up and runnin' on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server up and runnin' on port ${PORT}`));
